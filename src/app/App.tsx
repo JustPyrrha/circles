c1c1a0e65ff80a0b0f693d01c198ae3a281e2993
Pyrrha <pyrrha.social@gmail.com>
2022-01-10 23:10:32 +1300

    feat: background rings

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import "./App.sass"
 
 function App() {
     return (
-        <>
+        <div>
+            <div className="Item">
+                <div className="BackgroundRing">
+                    <div className="CenterDisc">
 
-        </>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
