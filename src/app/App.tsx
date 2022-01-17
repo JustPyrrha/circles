984d3686ca1e8a1168249c58df22fd7e8fd4ec67
Pyrrha <pyrrha.social@gmail.com>
2022-01-17 19:11:33 +1300

    feat: layout + circle text

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,18 +2,34 @@ import React from "react";
 import "./App.scss"
 import SegmentRing from "./components/SegmentRing/SegmentRing";
 import {MagnifierIcon, VisitArrowIcon} from "./icons";
+import CurvedText from "./components/CurvedText/CurvedText";
 
 function App() {
     const [countBroken, setCountBroken] = React.useState<number>(2)
     const [countLit, setCountLit] = React.useState<number>(4)
     const [countUnlit, setCountUnlit] = React.useState<number>(2)
+    const [text, setText] = React.useState<string>("Circle Name")
+
     return (
         <div>
             <div className="Item">
                 <div className="BackgroundRing">
                     <SegmentRing broken={countBroken} lit={countLit} unlit={countUnlit} />
+                    <CurvedText text={text}/>
                     <div className="CenterDisc">
+                        <div className="GridContainer">
+                            <div className="GridItem">
 
+                            </div>
+                        </div>
+                        <div className="GridContainer">
+                            <div className="GridItem">
+                                <MagnifierIcon />
+                            </div>
+                            <div className="GridItem">
+                                <VisitArrowIcon />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -26,6 +42,8 @@ function App() {
                 <br/>
                 <label htmlFor="#unlit-range">{countUnlit}</label>
                 <input id={"lit-range"} type={"range"} max={"10"} min={0} step={1} value={countUnlit} onInput={e => setCountUnlit(Number(e.currentTarget.value))}/>
+                <br/>
+                <input type={"text"} value={text} onInput={e => setText(e.currentTarget.value)} />
             </div>
         </div>
     );
---
+++ src/app/components/CurvedText/CurvedText.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const CurvedText = (props: { text: string }) => {
+    return (
+        <svg
+            viewBox={"0 0 512 512"}
+            preserveAspectRatio={"xMidYMid meet"}
+            style={{
+                transform: "rotate(90deg)",
+                zIndex: 10
+            }}
+        >
+            <defs>
+                <circle id={"text-curve"} cx={256} cy={256} r={128}/>
+            </defs>
+            <text style={{
+                fontSize: 25,
+                textAlign: "center",
+                fill: "white",
+                fontFamily: "arial"
+            }}>
+                <textPath xlinkHref={"#text-curve"} startOffset={"50%"} textAnchor={"middle"}>
+                    {props.text}
+                </textPath>
+            </text>
+        </svg>
+    )
+}
+
+export default CurvedText;
